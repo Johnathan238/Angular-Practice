2fb623078a0fe3173ba597a337f54d355782684e
Johnathan238 <jonathanumana86@gmail.com>
2022-07-28 18:42:25 -0400

    Learned hoe two-way-binding works

--- angular-first-app/src/app/server/server.component.ts
+++ angular-first-app/src/app/server/server.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServerComponent implements OnInit {
   allowNewServer = false
   serverCreationStatus = "No server was created!"
+  serverName = ''
 
   serverId = 10
   serverStatus = 'offline '
@@ -23,5 +24,9 @@ export class ServerComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created'
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value
   }
 }
