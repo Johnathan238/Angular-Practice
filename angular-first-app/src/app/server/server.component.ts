01932589fe27eecf9a02612b546d1022c5e71cb7
Johnathan238 <jonathanumana86@gmail.com>
2022-07-30 19:31:19 -0400

    Enchancing ngIf with an else condition

--- angular-first-app/src/app/server/server.component.ts
+++ angular-first-app/src/app/server/server.component.ts
@@ -9,9 +9,9 @@ export class ServerComponent implements OnInit {
   allowNewServer = false
   serverCreationStatus = "No server was created!"
   serverName = ''
-
   serverId = 10
   serverStatus = 'offline '
+  serverCreated = false
 
   constructor() {
     setTimeout(() => {
@@ -23,7 +23,8 @@ export class ServerComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created'
+    this.serverCreated = true
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
   }
 
   onUpdateServerName(event: any) {
